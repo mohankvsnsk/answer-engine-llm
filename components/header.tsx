27244997891593b7f01e7a2238e88cf4526f0c25
Mohan Kocherlakota <48925698+mohankvsnsk@users.noreply.github.com>
2024-04-03 17:13:43 -0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ export async function Header() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full px-4 border-b h-14 shrink-0 dark:bg-slate-800 bg-white backdrop-blur-xl">
       <span className="inline-flex items-center home-links whitespace-nowrap">
-        <a href="https://developersdigest.tech" rel="noopener" target="_blank">
+        <a href="https://mohankvsnsk.github.io/" rel="noopener" target="_blank">
           <span className="block sm:inline text-lg sm:text-xl lg:text-2xl font-semibold dark:text-white text-black">answer engine</span>
         </a>
       </span>
@@ -15,7 +15,7 @@ export async function Header() {
         <Button variant="outline" asChild>
           <a
             target="_blank"
-            href="https://git.new/answr"
+            href="https://github.com/mohankvsnsk/answer-engine-llm.git"
             rel="noopener noreferrer"
             className="bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 text-black dark:text-white font-semibold py-2 px-4 rounded shadow"
           >
